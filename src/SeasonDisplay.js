1a8df5f63f2012a34e6c318a32adaa5aba2c85f5
Sho Ito <i.sho.0628@gmail.com>
2019-03-17 19:04:32 +0900

    add icons

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -12,8 +12,15 @@ const getSeason = (lat, month) => {
 const SeasonDisplay = props => {
   const season = getSeason(props.lat, new Date().getMonth());
   const text = season === 'winter' ? 'Burr, it it chilly' : 'Lets hit the beatch';
+  const icon = season === 'winter' ? 'snowflake' : 'sun';
 
-  return <div>{text}</div>;
+  return (
+    <div>
+    <i className={`${icon} icon`} />
+      <h1>{text}</h1>
+    <i className={`${icon} icon`} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
